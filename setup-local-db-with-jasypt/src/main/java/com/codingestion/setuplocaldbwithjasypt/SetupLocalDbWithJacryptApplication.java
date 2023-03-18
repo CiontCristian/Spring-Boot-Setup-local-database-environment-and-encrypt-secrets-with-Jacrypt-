31de2c616fda11075b6a68a190dcd020e483c6b0
Cristi <cristian.ciont@gmail.com>
2023-03-18 20:40:34 +0200

    added application

---
+++ setup-local-db-with-jasypt/src/main/java/com/codingestion/setuplocaldbwithjasypt/SetupLocalDbWithJacryptApplication.java
@@ -0,0 +1,13 @@
+package com.codingestion.setuplocaldbwithjasypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetupLocalDbWithJacryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetupLocalDbWithJacryptApplication.class, args);
+    }
+
+}
